c12f069d9a90dde6ca0867ab277a4c2c1da006c3
Malhar150 <malharjardosh@gmail.com>
2025-08-12 23:20:40 +0530

    Add files via upload

---
+++ C/1st sem/P1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    int main(){
+        printf("##########################################################################\n");
+        printf("\t\t\t\tWELCOME!!\n");
+        printf("Please choose a number from the following options:\n");
+        printf("1.\tPlay the game\n");
+        printf("2.\nDemo the game\n");
+        printf("3.\tExit\n");
+        printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    return 0;
+}
